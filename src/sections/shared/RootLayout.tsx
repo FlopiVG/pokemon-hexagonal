2e67affc9d5d7d12ded365f25d6358fa339411bd
Florian <flopivillagarcia@gmail.com>
2024-05-04 13:25:16 +0200

    Add simple microfrontend connection

--- src/sections/shared/RootLayout.tsx
+++ src/sections/shared/RootLayout.tsx
@@ -1,49 +1,57 @@
+import { Suspense } from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
 import Typography from './Typography';
 import NavItem from './NavItem';
+import Navbar from 'navbar/Navbar';
 
 const RootLayout = () => {
+  console.log(Navbar);
   return (
-    <div className="flex gap-8">
-      <div className="w-1/6">
-        <Typography as="h1" variant="title" className="text-center">
-          Pokédex
-        </Typography>
-        <nav className="m-4">
-          <ul>
-            <NavLink to="/">
-              {({ isActive }) => (
-                <NavItem active={isActive} iconName="house">
-                  Home
-                </NavItem>
-              )}
-            </NavLink>
-            <NavLink to="/pokemon">
-              {({ isActive }) => (
-                <NavItem active={isActive} iconName="box">
-                  Pokemon
-                </NavItem>
-              )}
-            </NavLink>
-            <NavLink to="/trainer">
-              {({ isActive }) => (
-                <NavItem active={isActive} iconName="people">
-                  Trainers
-                </NavItem>
-              )}
-            </NavLink>
-            <NavLink to="/analytic">
-              {({ isActive }) => (
-                <NavItem active={isActive} iconName="analytics">
-                  Analytics
-                </NavItem>
-              )}
-            </NavLink>
-          </ul>
-        </nav>
-      </div>
-      <div className="w-5/6 bg-slate-200 p-8">
-        <Outlet />
+    <div>
+      <Suspense fallback="loading">
+        <Navbar />
+      </Suspense>
+      <div className="flex gap-8">
+        <div className="w-1/6">
+          <Typography as="h1" variant="title" className="text-center">
+            Pokédex
+          </Typography>
+          <nav className="m-4">
+            <ul>
+              <NavLink to="/">
+                {({ isActive }) => (
+                  <NavItem active={isActive} iconName="house">
+                    Home
+                  </NavItem>
+                )}
+              </NavLink>
+              <NavLink to="/pokemon">
+                {({ isActive }) => (
+                  <NavItem active={isActive} iconName="box">
+                    Pokemon
+                  </NavItem>
+                )}
+              </NavLink>
+              <NavLink to="/trainer">
+                {({ isActive }) => (
+                  <NavItem active={isActive} iconName="people">
+                    Trainers
+                  </NavItem>
+                )}
+              </NavLink>
+              <NavLink to="/analytic">
+                {({ isActive }) => (
+                  <NavItem active={isActive} iconName="analytics">
+                    Analytics
+                  </NavItem>
+                )}
+              </NavLink>
+            </ul>
+          </nav>
+        </div>
+        <div className="w-5/6 bg-slate-200 p-8">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'pokemon-hexagonal',
+      remotes: {
+        navbar: 'http://localhost:5173/assets/remoteEntry.js',
+      },
+      shared: ['react', 'react-dom'],
+    }),
+  ],
   preview: {
     port: 8080,
     strictPort: true,
@@ -14,5 +24,11 @@ export default defineConfig({
     strictPort: true,
     host: true,
     origin: 'http://0.0.0.0:8080',
+  },
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
   },
 });
